c64f7a77316285754dd35cf9ef654c6132ef3cee
Luke Busfield <luke05busfield@gmail.com>
2023-06-17 11:36:35 +0200

    created the grid. blue div for testing.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
@@ -28,3 +29,42 @@ container.appendChild(content);
 container.appendChild(red);
 container.appendChild(h3);
 container.appendChild(div);
+
+const container = document.querySelector('#container');
+
+let columns = new Array(16);
+columns.forEach(function(){
+    columns[i] = new Array(16);
+    columns[i].forEach(function(){
+        let div = document.createElement("div");
+        div.style.color = black;
+    });
+    container.append(columns[i]);
+});
+*/
+const container = document.querySelector('#container');
+const columns = new Array(16);
+
+for(i = 0; i < 16; i++){
+    columns[i] = document.createElement('div');
+    columns[i].style.display = "flex";
+    columns[i].style.height = "6.25%";
+    columns[i].style.gap = "5px";
+    columns[i].style.marginBottom = "5px";
+
+    let row = new Array(16);
+
+    for(j = 0; j < 16; j++){
+        let div = document.createElement('div');
+        div.style.backgroundColor = "black";
+        div.style.height = "100%";
+        div.style.gap = "5px";
+        div.style.width = "6.25%";
+        row[j] = div;
+        columns[i].appendChild(div);
+    }
+
+    container.appendChild(columns[i]);
+}
+
+columns[5].childNodes[4].style.backgroundColor = "blue";
