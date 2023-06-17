9b9fd2a09f4fb3f29cafbb126c3ea8d3548c6906
Luke Busfield <luke05busfield@gmail.com>
2023-06-17 13:48:00 +0200

    completed base tasks. Need to look at container.

--- script.js
+++ script.js
@@ -1,70 +1,60 @@
-/*
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+const bodyHeight = window.innerHeight;
+const containerHeight = bodyHeight * 0.8;
+container.style.height = containerHeight + 'px';
 
-const red = document.createElement('p');
-red.textContent = 'I am red!';
-red.style.color = "red";
+container.style.width = containerHeight + 'px';
 
-const h3 = document.createElement("h3");
-h3.textContent = "I’m a blue h3!";
-h3.style.color = "blue";
+const leftMargin = window.innerWidth * 0.5 - containerHeight * 0.5;
+container.style.marginLeft =  leftMargin+ 'px';
 
-const div = document.createElement("div");
-div.style.borderColor = "black";
-div.style.backgroundColor = "pink";
+createGrid(16, 16);
 
-const h1 = document.createElement("h1");
-h1.textContent = "I’m in a div";
-div.appendChild(h1);
+function createGrid(colsInput, rowsInput){
+    container.innerHTML = '';
+    const rows = new Array(colsInput);
+    for(i = 0; i < rowsInput; i++){
+        rows[i] = document.createElement('div');
+        rows[i].style.display = "flex";
+        const heightPercentage = 100/rowsInput;
+        rows[i].style.height = heightPercentage + "%";
 
-const p = document.createElement('p');
-p.textContent = 'ME TOO!';
-div.appendChild(p);
+        let column = new Array(colsInput);
 
-container.appendChild(content);
-container.appendChild(red);
-container.appendChild(h3);
-container.appendChild(div);
+        for(j = 0; j < colsInput; j++){
+            let div = document.createElement('div');
+            div.style.backgroundColor = "black";
+            div.style.height = "100%";
+            const widthPercentage = 100/colsInput;
+            div.style.width = widthPercentage + "%";
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = "red";
+            });
+            column[j] = div;
+            rows[i].appendChild(div);
+        }
 
-const container = document.querySelector('#container');
-
-let columns = new Array(16);
-columns.forEach(function(){
-    columns[i] = new Array(16);
-    columns[i].forEach(function(){
-        let div = document.createElement("div");
-        div.style.color = black;
-    });
-    container.append(columns[i]);
-});
-*/
-const container = document.querySelector('#container');
-const columns = new Array(16);
-
-for(i = 0; i < 16; i++){
-    columns[i] = document.createElement('div');
-    columns[i].style.display = "flex";
-    columns[i].style.height = "6.25%";
-    columns[i].style.gap = "5px";
-    columns[i].style.marginBottom = "5px";
-
-    let row = new Array(16);
-
-    for(j = 0; j < 16; j++){
-        let div = document.createElement('div');
-        div.style.backgroundColor = "black";
-        div.style.height = "100%";
-        div.style.gap = "5px";
-        div.style.width = "6.25%";
-        row[j] = div;
-        columns[i].appendChild(div);
+        container.appendChild(rows[i]);
     }
-
-    container.appendChild(columns[i]);
 }
 
-columns[5].childNodes[4].style.backgroundColor = "blue";
+const btn = document.querySelector("button");
+btn.addEventListener('click', () => {
+    /*
+    for(i = 0; i < 16; i++){
+        for(j = 0; j < 16; j++){
+            columns[i].childNodes[j].style.backgroundColor = "black";   
+        }
+    }
+    */
+   const colsInput = prompt("How many columns do you want?");
+   const rowsInput = prompt("How many rows do you want?");   
+   if(isNaN(colsInput) || isNaN(rowsInput)){
+    alert("invalid input");
+   }else if(colsInput > 100 || rowsInput > 100){
+    alert("Only 100 rows and columns allowed.")
+   }else{
+    createGrid(colsInput, rowsInput);
+   }
+});
